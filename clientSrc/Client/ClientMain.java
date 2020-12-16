d8c51853c848fd3e4bd17de0486746e6a3394893
pancycka <pancycka@BY1926A75000144.wit.private>
2020-12-15 23:42:47 -0500

    Final change to ClientMain

--- clientSrc/Client/ClientMain.java
+++ clientSrc/Client/ClientMain.java
@@ -13,11 +13,10 @@ public class ClientMain {
 	public static void main(String[] args)
 	{
 		ClientApplication client = new ClientApplication();
-		
 		client.setTitle("Checkers Player");
+		client.setLocation(200, 100);
 		client.pack();
 		client.setVisible(true);
-		client.setLocation(200, 100);
 		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
